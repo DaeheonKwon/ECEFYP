8e27b666d75ae09ff6de75597801331adcde6446
DaeheonKwon <scientist617@snu.ac.kr>
2024-04-27 12:40:10 +0900

    deleted irrelevent files

--- myFT.m
+++
@@ -1,5 +0,0 @@
-function freq = myFT(img)
-    
-    freq = fftshift(fftshift(fft2(img),1),2);
-    
-end
--- myIFT.m
+++
@@ -1,3 +0,0 @@
-function img = myIFT(freq)
-    img = ifft2(ifftshift(ifftshift(freq,1),2));
-end
